067a4d08e9794f64ba98a9cf3d1d8ead287a4563
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2021-06-21 11:00:07 +0300

    :ok_hand: add the license and spotless to maintain formating and update licensing on the project files

--- src/main/java/autoconfigure/SecurityAutoConfiguration.java
+++ src/main/java/autoconfigure/SecurityAutoConfiguration.java
@@ -1,24 +1,20 @@
-package autoconfigure;
-
-/*-
- * #%L
- * hapi-fhir-spring-boot-autoconfigure
- * %%
- * Copyright (C) 2014 - 2021 Smile CDR, Inc.
- * %%
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
+
+package autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -32,9 +28,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  * @author Reham Muzzamil
  */
 @Configuration
-@AutoConfigureAfter({ KeycloakSecurityConfig.class })
+@AutoConfigureAfter({KeycloakSecurityConfig.class})
 @EnableWebSecurity
-@Import({ KeycloakSecurityConfig.class })
-public class SecurityAutoConfiguration {
-
-}
+@Import({KeycloakSecurityConfig.class})
+public class SecurityAutoConfiguration {}
